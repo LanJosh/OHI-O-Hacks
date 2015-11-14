f6f5b01331b24dcd9328484f8ab278d6cd8513fd
AaronPycraft <aapyc1@gmail.com>
2015-11-14 16:46:21 -0500

    Got the file reading process working for student data! - Aaron 4:46 pm 11/14/2015

--- TDI Challenge/src/StudentDataParser.java
+++ TDI Challenge/src/StudentDataParser.java
@@ -1,32 +1,45 @@
 
 import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class StudentDataParser {
+public class StudentDataParser implements IParser{
   //--Variables
+  private ArrayList <String[]> studentInfo;
+  private File file;
+  
+  //--Constructor
+  public StudentDataParser(File file) {
+		studentInfo = new ArrayList<String[]>();
+		this.file = file;
+	}//end constructor
+  
   //--Methods
-  void parse() {
-    //--Establish communication with file
-    File currDir = new File(".");
-    File parentDir = currDir.getParentFile();
-    File file = new File(parentDir,"/Documents/TDA Students Test.csv");
-    
-    //--Create object to read file
-    try {
-      Scanner reader = new Scanner(file);
-      reader.useDelimiter("^ \"");
-    
-      if(reader.hasNext()) 
-        System.out.println("Next word:\t" + reader.next());
+  public ArrayList<String[]> parse(File file) {
+	  try {
+		  
+		  Scanner fIn = new Scanner(file);
+		  fIn.useDelimiter("^");
+		  
+		  
+          //--Loop through the entire file
+		  while (fIn.hasNext()) {
+			  System.out.println(fIn.next());
+        	  
+              
+          }//end loop
+		  fIn.close();
+      }//end try 
+	  catch (FileNotFoundException e) {
+          System.out.println("FILE NOT FOUND");
+          e.printStackTrace();
+      }//end catch
       
-      reader.close();
-    }//end try 
-    catch(FileNotFoundException e) {
-      e.printStackTrace();
-      System.out.println("FILE NOT FOUND");
-    }//end catch
+    return studentInfo;
+    }//--end method
 
-  }//--end method
+}//end class
 
 
-}
+
