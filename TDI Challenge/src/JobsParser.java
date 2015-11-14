f781a1afc5658865069d18a1e4305fe0981f1084
Joshua Lan <josh.lan9@gmail.com>
2015-11-14 15:05:01 -0500

    Implementation for Job Parser

--- TDI Challenge/src/JobsParser.java
+++ TDI Challenge/src/JobsParser.java
@@ -1,7 +1,42 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class JobsParser {
+/*
+ * Parses ^ delimited file containing job information
+ * 
+ * 1459 Joshua Lan
+ */
+public class JobsParser implements IParser  {
+
+	public ArrayList <String[]> allJobInfo;
+	public File data;
+	
+	public JobsParser(File data){
+		allJobInfo = new ArrayList<String[]>();
+		this.data = data;
+	}
+	
+    public ArrayList<String[]> parse(File data) {
+        try (Scanner sc = new Scanner(data)){
+            sc.useDelimiter("^");
+
+            // Data contains 5 columns
+            while (sc.hasNext()){
+                for (int i = 0; i < 5; i++) {
+                String[] jobInfo = new String[5];
+                String descriptionData = sc.next(); 
+                System.out.print(descriptionData + "|");
+                jobInfo[i] = descriptionData;
+                allJobInfo.add(jobInfo);
+                System.out.println();
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("File Not Found");
+        }
+        
+    return allJobInfo;
+    }
 
 }
