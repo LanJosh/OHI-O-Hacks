782f415759f5d97da7401bbd885f0fdc460a2602
Joshua Lan <josh.lan9@gmail.com>
2015-11-15 07:13:47 -0500

    Analysis tools

---
+++ TDA Challenge/src/KeywordDictionary.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.*;
+/*
+ * Reads from DimensionDictionary files. Creates mapping to dimension from keywords
+ * 
+ * 0459 Joshua Lan
+ */
+public class KeywordDictionary {
+	protected Map<String, Set<String>> DimensionDict;
+	
+	public KeywordDictionary(){
+		 // TODO make dictionary from text file
+	}
+	
+}
